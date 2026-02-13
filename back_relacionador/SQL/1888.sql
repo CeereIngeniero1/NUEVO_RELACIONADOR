b005ffe6f55f37754c8f878749637d0811cc975d
Theoclas <Fernando.pala.99@gmail.com>
2026-02-13 10:42:32 -0500

    Sebas no tiene almuerzo

--- back_relacionador/SQL/1888.sql
+++ back_relacionador/SQL/1888.sql
@@ -49,3 +49,87 @@ alter table Entidad1888
  add constraint fg_Discapacidad
  foreign key ([Id Discapacidad])
  references Discapacidad
+
+
+
+-- Insert para tablas 
+
+INSERT INTO [dbo].[Sexo Identidad Genero]
+           ([Codigo]
+           ,[Identidad Genero]
+           ,[Descripcion Identidad Genero]
+           ,[Id Estado])
+     VALUES
+           ('01',
+           'Masculino',
+           'Masculino',
+           '7')
+           ,
+           ('02',
+           'Femenino',
+           'Femenino',
+           '7')
+           ,
+           ('03',
+           'Transgénero',
+           'Transgénero',
+           '7')
+           ,
+           ('04',
+           'Neutro',
+           'Neutro',
+           '7')
+           ,
+           ('05',
+           'No lo declara',
+           'No lo declara',
+           '7')
+GO
+
+
+
+update etnia set [Id Estado] = 8
+
+INSERT INTO [dbo].[Etnia]
+           ([Código Etnia]
+           ,[Etnia]
+           ,[Descripción Etnia]
+           ,[Orden Etnia]
+           ,[Id Estado])
+     VALUES
+           ('1',
+           'Indigena',
+           'Indigena',
+           1,
+           '7')
+           ,
+           ('2',
+           'ROM (Gitano)',
+           'ROM (Gitano)',
+           1,
+           '7')
+           ,
+           ('3',
+           'Raizal (Archipielago San Andrés y Providencia)',
+           'Raizal (Archipielago San Andrés y Providencia)',
+           1,
+           '7')
+           ,
+           ('4',
+           'Palenquero de San Basilio',
+           'Palenquero de San Basilio',
+           1,
+           '7')
+           ,
+           ('5',
+           'Negro(a) o mulato(a) o afrocolombiano(a) o afrodescendiente',
+           'Negro(a) o mulato(a) o afrocolombiano(a) o afrodescendiente',
+           1,
+           '7')
+           ,
+           ('6',
+           'Otras etnias',
+           'Otras etnias',
+           1,
+           '7')
+GO
